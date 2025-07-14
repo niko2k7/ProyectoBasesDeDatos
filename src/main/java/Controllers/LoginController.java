96677c229d6ced8d69c9c672dd12f1ced71b9011
niko2k7 <nicolas.aguirre2k7@gmail.com>
2025-07-14 00:32:39 -0500

    cambios en la versión de java usada

--- src/main/java/Controllers/LoginController.java
+++ src/main/java/Controllers/LoginController.java
@@ -1,14 +1,13 @@
 package Controllers;
 
 import DataBase.DataBaseConnection;
-import java.sql.Connection;
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController {
