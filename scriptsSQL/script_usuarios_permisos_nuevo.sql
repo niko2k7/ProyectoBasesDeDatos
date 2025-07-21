1b77f1f34f81e9e54a5f65079ffc63b36b073b3c
nicolasmcort <evmunoz@unal.edu.co>
2025-07-21 08:44:13 -0500

    Update script_usuarios_permisos_nuevo.sql

--- scriptsSQL/script_usuarios_permisos_nuevo.sql
+++ scriptsSQL/script_usuarios_permisos_nuevo.sql
@@ -24,6 +24,7 @@ CREATE ROLE 'rol_almacen_taller'@'localhost';
 
 
 -- 3. Asignar permisos a cada ROL
+
 -- 3.1. Permisos para 'rol_administrador'
 -- Acceso total a todas las funcionalidades y datos del sistema.
 GRANT ALL PRIVILEGES ON proyecto_base_datos.* TO 'rol_administrador'@'localhost';
@@ -31,62 +32,27 @@ GRANT ALL PRIVILEGES ON proyecto_base_datos.* TO 'rol_administrador'@'localhost'
 -- 3.2. Permisos para 'rol_gerente'
 -- Visión completa de operaciones (reportes, análisis). Solo lectura.
 GRANT SELECT ON proyecto_base_datos.* TO 'rol_gerente'@'localhost';
+GRANT DELETE ON proyecto_base_datos.CLIENTE TO 'rol_gerente'@'localhost';
+GRANT DELETE ON proyecto_base_datos.PROOVEDOR TO 'rol_gerente'@'localhost';
+GRANT DELETE ON proyecto_base_datos.EMPLEADO TO 'rol_gerente'@'localhost';
+GRANT DELETE ON proyecto_base_datos.HORARIO_EMPLEADO TO 'rol_gerente'@'localhost';
+
 
 -- 3.3. Permisos para 'rol_ventas'
 -- Registro de facturas de venta, consulta de productos, gestión de clientes, seguimiento de cuentas por cobrar.
 -- No acceso a costos de compra o salarios.
 GRANT SELECT, INSERT, UPDATE ON proyecto_base_datos.ACTOR TO 'rol_ventas'@'localhost';
 GRANT SELECT, INSERT, UPDATE ON proyecto_base_datos.CLIENTE TO 'rol_ventas'@'localhost';
-GRANT SELECT, INSERT ON proyecto_base_datos.FACTURA_VENTA TO 'rol_ventas'@'localhost';
-GRANT SELECT, INSERT ON proyecto_base_datos.DETALLE_FACTURA_VENTA TO 'rol_ventas'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON proyecto_base_datos.FACTURA_VENTA TO 'rol_ventas'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON proyecto_base_datos.DETALLE_FACTURA_VENTA TO 'rol_ventas'@'localhost';
 GRANT SELECT ON proyecto_base_datos.PRODUCTO TO 'rol_ventas'@'localhost'; -- Solo consulta de productos (incluye precios de venta)
-GRANT SELECT ON proyecto_base_datos.SERVICIO TO 'rol_ventas'@'localhost';   -- Para consultar stock
-GRANT SELECT, UPDATE ON proyecto_base_datos.ARTICULO TO 'rol_ventas'@'localhost';   -- Para consultar stock
-GRANT SELECT ON proyecto_base_datos.CUENTA_POR_COBRAR TO 'rol_ventas'@'localhost'; -- Creación de cuentas por cobrar al vender a crédito
-GRANT SELECT ON proyecto_base_datos.PLAZO_COBRO TO 'rol_ventas'@'localhost'; -- Registrar plazos y actualizar estados de cuotas
+GRANT SELECT ON proyecto_base_datos.ARTICULO TO 'rol_ventas'@'localhost';   -- Para consultar stock
+GRANT SELECT, INSERT ON proyecto_base_datos.CUENTA_POR_COBRAR TO 'rol_ventas'@'localhost'; -- Creación de cuentas por cobrar al vender a crédito
+GRANT SELECT, INSERT, UPDATE ON proyecto_base_datos.PLAZO_COBRO TO 'rol_ventas'@'localhost'; -- Registrar plazos y actualizar estados de cuotas
 
 -- Permisos de VISTAS específicos para 'rol_ventas'
 GRANT SELECT ON proyecto_base_datos.Vista_Inventario_Consulta TO 'rol_ventas'@'localhost';
 GRANT SELECT ON proyecto_base_datos.Vista_Cuentas_Cobrar_Resumen TO 'rol_ventas'@'localhost';
-
--- Permisos de PA
--- Permisos de GRANT para rol_ventas@localhost
-
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_obtener_clientes TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_insertar_cliente TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_actualizar_actor TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_eliminar_cliente TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_crear_factura_venta TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_agregar_detalle_factura TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_obtener_articulos TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_obtener_servicios TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_eliminar_articulo TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_eliminar_servicio TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_articulos_por_tipo TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_articulos_por_marca TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_articulos_por_modelo TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_filtrar_stock_critico TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_filtrar_sin_stock TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_obtener_facturas_venta TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_factura_por_codigo TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_facturas_por_fecha TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_facturas_por_intervalo TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_factura_por_documento TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_filtrar_facturas_efectivo TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_filtrar_facturas_tarjeta TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_filtrar_facturas_transferencia TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_obtener_datos_cliente TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_cargar_articulos_factura TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_cargar_servicios_factura TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_obtener_actor_por_documento TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_cargar_cuentas_por_cobrar TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_cuenta_por_id TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_cuenta_por_fecha_emision TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_buscar_cuenta_por_documento TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_filtrar_cuentas_en_curso TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_filtrar_cuentas_vencidas TO 'rol_ventas'@'localhost';
-GRANT EXECUTE ON PROCEDURE proyecto_base_datos.sp_filtrar_cuentas_pagadas TO 'rol_ventas'@'localhost';
-
 
 
 -- 3.4. Permisos para 'rol_contabilidad'
@@ -104,12 +70,20 @@ GRANT SELECT, INSERT, UPDATE ON proyecto_base_datos.PLAZO_COBRO TO 'rol_contabil
 GRANT SELECT, INSERT, UPDATE ON proyecto_base_datos.CUENTA_POR_PAGAR TO 'rol_contabilidad'@'localhost';
 GRANT SELECT, INSERT, UPDATE ON proyecto_base_datos.PLAZO_PAGO TO 'rol_contabilidad'@'localhost';
 GRANT SELECT ON proyecto_base_datos.CLIENTE TO 'rol_contabilidad'@'localhost'; -- Consulta de clientes para conciliaciones
+GRANT DELETE ON proyecto_base_datos.PROOVEDOR TO 'rol_contabilidad'@'localhost';
+GRANT DELETE ON proyecto_base_datos.FACTURA_COMPRA TO 'rol_contabilidad'@'localhost';
+GRANT DELETE ON proyecto_base_datos.DETALLE_FACTURA_COMPRA TO 'rol_contabilidad'@'localhost';
+GRANT DELETE ON proyecto_base_datos.CUENTA_POR_COBRAR TO 'rol_contabilidad'@'localhost';
+GRANT DELETE ON proyecto_base_datos.CUENTA_POR_PAGAR TO 'rol_contabilidad'@'localhost';
+
+
 
 -- Permisos de VISTAS específicos para 'rol_contabilidad'
 GRANT SELECT ON proyecto_base_datos.Vista_Ventas_Historico TO 'rol_contabilidad'@'localhost';
 GRANT SELECT ON proyecto_base_datos.Vista_Cuentas_Cobrar_Resumen TO 'rol_contabilidad'@'localhost';
 GRANT SELECT ON proyecto_base_datos.Vista_Cuentas_Pagar_Resumen TO 'rol_contabilidad'@'localhost';
 GRANT SELECT ON proyecto_base_datos.Vista_Nomina_Resumen TO 'rol_contabilidad'@'localhost';
+
 
 -- 3.5. Permisos para 'rol_almacen_taller'
 -- Gestión y consulta de materiales y herramientas, registro de prestación de servicios.
@@ -119,6 +93,7 @@ GRANT SELECT, UPDATE ON proyecto_base_datos.ARTICULO TO 'rol_almacen_taller'@'lo
 GRANT SELECT ON proyecto_base_datos.SERVICIO TO 'rol_almacen_taller'@'localhost'; -- Consultar tipos de servicio
 GRANT SELECT, INSERT ON proyecto_base_datos.SERVICIO_HAS_EMPLEADO TO 'rol_almacen_taller'@'localhost'; -- Registrar que un empleado prestó un servicio (si esta es la tabla de registro)
 GRANT SELECT ON proyecto_base_datos.DETALLE_FACTURA_VENTA TO 'rol_almacen_taller'@'localhost'; -- Si necesitan verificar detalles de una venta de servicio
+GRANT DELETE ON proyecto_base_datos.SERVICIO TO 'rol_contabilidad'@'localhost';
 
 -- Permisos de VISTAS específicos para 'rol_almacen_taller'
 GRANT SELECT ON proyecto_base_datos.Vista_Inventario_Consulta TO 'rol_almacen_taller'@'localhost';
@@ -154,4 +129,3 @@ SET DEFAULT ROLE ALL TO
 
 -- 7. Refrescar los privilegios para que los cambios surtan efecto
 FLUSH PRIVILEGES;
-
